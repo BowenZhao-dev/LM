7beb89cbcaff189a92bd461e551401e69fe99277
Bowen Zhao <94955173+BowenZhaoASIR@users.noreply.github.com>
2022-03-04 22:41:56 +0100

    Add files via upload

--- Bowen-Zhao-Pratica1-UT5/Bowen-Zhao-pratica1-UT5-eje4.js
+++ Bowen-Zhao-Pratica1-UT5/Bowen-Zhao-pratica1-UT5-eje4.js
@@ -1,15 +1,18 @@
 //Ejercicio 4
 //const Dact = prompt("dia actual")
-const Mact = prompt("mes actual")
-const Aact = prompt("año actual")
+'use strict';
+let Kilos = prompt("kilos de caramelos quieres?, solo puedes coger 1, 2 y 3 no mas de 3")
 
-//const Dnc = prompt("dia nace")
-const Mnc = prompt("mes nace")
-const Anc = prompt("año nace")
-let res= Aact - Anc
-let resM= Mnc - Mact 
-if (res > 65 , res = 65 )
-if ( resM = 0 , resM < 0 )
-alert ("yes")
-else if (res < 65)
-alert("No")
+if (Kilos == 1){
+        alert("1 kilo de caramelos, otal de 25 de caramelos de 40 g")
+    }
+
+if (Kilos == 2){
+        alert("2 kilo de caramelos, total de 50 de caramelos de 20 g y 25 de caramelos de 40 g")
+    }
+
+if (Kilos == 3){
+        alert("3 kilo de caramelos, total de 100 caramelos de 10 g 25 de 40 g y 50 de 20 g")
+    }
+
+
